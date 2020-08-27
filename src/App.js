f18c978bce47dbb3649b7e4cec8a2abba3667ad2
Moriah Humphries <moriah.humphries@gmail.com>
2020-08-27 12:02:13 -0500

    set up handlers for text input changes

--- src/App.js
+++ src/App.js
@@ -1,40 +1,63 @@
-import React from "react";
+import React, {setState, useState} from "react";
 import "./index.css";
 
 export default function App() {
+    const [values, setValues] = useState({
+        firstName: "",
+        lastName: "",
+        email: ""
+    });
+
+    const [submitted, setSubmitted] = useState(
+    )
+
+    const handleFirstNameChange = (event) => {
+        setValues({...values, firstName: event.target.value})
+    }
+    const handleLastNameChange = (event) => {
+        setValues({...values, lastName: event.target.value})
+    }
+    const handleEmailChange = (event) => {
+        setValues({...values, email: event.target.value})
+    }
+
   return (
-      <div class="form-container">
-        <form class="register-form">
-          {/* Uncomment the next line to show the success message */}
-          {/* <div class="success-message">Success! Thank you for registering</div> */}
+      <div className="form-container">
+        <form className="register-form">
+           <div className="success-message">Success! Thank you for registering</div>
           <input
               id="first-name"
-              class="form-field"
+              value={values.firstName}
+              className="form-field"
               type="text"
               placeholder="First Name"
               name="firstName"
+              onChange={handleFirstNameChange}
           />
-          {/* Uncomment the next line to show the error message */}
-          {/* <span id="first-name-error">Please enter a first name</span> */}
+           <span id="first-name-error">Please enter a first name</span>
           <input
               id="last-name"
-              class="form-field"
+              value={values.lastName}
+              className="form-field"
               type="text"
               placeholder="Last Name"
               name="lastName"
+              onChange={handleLastNameChange}
           />
-          {/* Uncomment the next line to show the error message */}
-          {/* <span id="last-name-error">Please enter a last name</span> */}
+
+           <span id="last-name-error">Please enter a last name</span>
           <input
               id="email"
-              class="form-field"
+              value={values.email}
+              className="form-field"
               type="text"
               placeholder="Email"
               name="email"
+              onChange={handleEmailChange}
           />
-          {/* Uncomment the next line to show the error message */}
-          {/* <span id="email-error">Please enter an email address</span> */}
-          <button class="form-field" type="submit">
+
+           <span id="email-error">Please enter an email address</span>
+          <button className="form-field" type="submit">
             Register
           </button>
         </form>
